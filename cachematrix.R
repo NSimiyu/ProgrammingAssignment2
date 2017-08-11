889550907bafbb7414f07c66670a5cc47debaf07
Nato Simiyu <natosimiyu@gmail.com>
2017-08-11 15:26:27 +0300

    Added functions
    Updated the base R script with assignment changes i.e. functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
 ## functions do
-
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        invm <- NULL
+        set <- function(y) {
+                x <<- y
+                invm <<- NULL
+        }
+        get <- function() x
+        setinv <- function() invm <<- solve(x)
+        getinv <- function() invm
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of the matrix x from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        invm <- x$getinv()
+        if(!is.null(invm)) {
+                message("getting cached inverse matrix")
+                return(invm)
+        }
+        matdata <- x$get()
+        invm <- solve(matdata, ...)
+        x$setinv(invm)
+        invm
 }
